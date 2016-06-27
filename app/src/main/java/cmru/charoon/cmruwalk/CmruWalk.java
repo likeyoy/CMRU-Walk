8a189ad3127e46f5926038a8c6081c1f5f489f29
likeyoy <gentlejimlee@gmail.com>
2016-06-27 11:38:32 +0700

    First set up

---
+++ app/src/main/java/cmru/charoon/cmruwalk/CmruWalk.java
@@ -0,0 +1,13 @@
+package cmru.charoon.cmruwalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CmruWalk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.walk_main);
+    }
+}
